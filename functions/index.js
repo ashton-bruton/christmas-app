376f825f4e53c7d8ea0ee2d790bbc95b260544eb
ashton-bruton <ashton.bruton@gmail.com>
2024-12-02 21:53:19 -0600

    Update

--- functions/index.js
+++ functions/index.js
@@ -3,24 +3,20 @@ const functions = require("firebase-functions");
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 const express = require('express');
-const app = express();
 const admin = require('firebase-admin');
 admin.initializeApp();
 require("dotenv").config();
-
 
 // Gmail API Configuration
 const CLIENT_ID = process.env.CLIENT_ID || functions.config().google.client_id;
 const CLIENT_SECRET = process.env.CLIENT_SECRET || functions.config().google.client_secret;
 const REDIRECT_URI = "https://christmas-app-e9bf7.web.app";
 const REFRESH_TOKEN = process.env.REFRESH_TOKEN || functions.config().google.refresh_token;
-const PORT = process.env.PORT || functions.config().app.port;
-const PROJECT_CONFIG = process.env.PROJECT_CONFIG || JSON.parse(functions.config().app.project_config);
 const API_KEY = process.env.API_KEY || functions.config().google.api_key;
 
 console.log("Client ID:", CLIENT_ID);
-console.log("Project Config:", PROJECT_CONFIG);
 
+// Initialize Google OAuth2 Client
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
   CLIENT_SECRET,
@@ -28,21 +24,16 @@ const oAuth2Client = new google.auth.OAuth2(
 );
 oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
-  
-  exports.api = functions.https.onRequest(app);
+const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${port}`);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-exports.helloWorld = functions.https.onRequest((req, res) => {
-    res.send("Hello, world!");
-  });
+// Export the Express app as a Firebase Function
+exports.api = functions.https.onRequest(app);
 
-// Firebase Function
+// Cloud Function for Sending Emails
 exports.sendCharacterEmail = functions.https.onCall(async (data, context) => {
   const { email, character, status } = data;
 
